a08c8055b0a109c4e24cc49d884eaa96a8a3024c
hiroghap1 <hiro_ghap1@yahoo.co.jp>
2021-07-28 17:08:16 +0900

    jQuery 画像呼び出し#

--- js/restapi-jquery.js
+++ js/restapi-jquery.js
@@ -4,15 +4,20 @@ $(function (){
 
     $.getJSON(url+'wp-json/wp/v2/posts?_embed&per_page='+posts)
         .done(function(json) {
-        // console.log(json);
+        console.log(json);
         const postNum = json.length;
         let html = '';
         for(let i = 0; i < postNum; i++) {
             const date = new Date(json[i].date);
+            const image = json[i]['_embedded']['wp:featuredmedia'];
             html += '<li class="news-item"><div class="news-item__meta"><time datetime="'+
                 date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2) + '">' +
                 date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() +'日' +
-                '</time></div><h3 class="news-item__title">' + json[i].title.rendered + '</h3></li>';
+                '</time></div><h3 class="news-item__title">' + json[i].title.rendered + '</h3>';
+            if(image) {
+                html += '<img src="' + image[0]['source_url'] + '" alt="">';
+            }
+            html += '</li>';
         }
         $('#news-list-jquery').html(html);
     }).fail(function(jqXHR, textStatus, errorThrown) {
